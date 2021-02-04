f736cf3939048857576fcb8542bfea6107c39bb6
Dharun Sivakumar <dharunvs@users.noreply.github.com>
2021-02-04 23:02:59 +0530

    Update pos_gui.py

--- pos_gui.py
+++ pos_gui.py
@@ -62,15 +62,15 @@ class POS:
         self.frame3 = Frame(self.window, height=self.H//3,
                             width=2*(self.W)//3, relief=RIDGE, bd=3, bg=BACKGROUND)
 
-        self.bill_frame = Frame(self.frame1, relief=RIDGE, bd=10, height=(2*self.H)//3,
+        self.bill_frame = Frame(self.frame1, relief=RIDGE, height=(2*self.H)//3,
                                 width=2*(self.W)//3, bg=BACKGROUND)
         self.items_frame = Frame(self.frame2, relief=RIDGE, bd=10, height=self.H,
                                  width=self.W//3, bg=BACKGROUND)
-        self.control_frame = Frame(self.frame3, relief=RIDGE, bd=10, height=self.H//3,
+        self.control_frame = Frame(self.frame3, relief=RIDGE, height=self.H//3,
                                    width=2*(self.W)//3, bg=BACKGROUND)
 
-        self.types_frame = Frame(self.frame2, relief=RIDGE, bd=10, height=self.H,
-                                 width=self.W//3, bg=BACKGROUND)
+        self.types_frame = Frame(self.frame2, relief=RIDGE, bd=6,
+                                 width=(self.W//3)-8, bg=BACKGROUND)
 
         cuisines = ["INDIAN", "CHINESE", "ITALIAN",
                     "ARABIC", "CHAT", "DESSERTS"]
@@ -79,14 +79,14 @@ class POS:
         for i in range(len(cuisines)):
             self.type = Button(
                 self.types_frame, text=cuisines[i], font=FONT, width=16, bg=BACKGROUND,
-                fg=FOREGROUND, command=lambda j=cuisines[i]: self.add_items_display(j))
+                fg=YELLOW, command=lambda j=cuisines[i]: self.add_items_display(j))
 
             c = 0
             if i % 2 == 0:
                 r += 1
             else:
                 c = 1
-            self.type.grid(row=r, column=c, pady=5, padx=5)
+            self.type.grid(row=r, column=c, pady=12, padx=15)
 
         self.add_items_display("CHINESE")
 
@@ -95,9 +95,10 @@ class POS:
         self.frame3.grid(row=1, column=0)
 
         self.items_frame.place(x=0, y=0)
-        self.types_frame.place(x=0, y=200)
+        self.types_frame.place(x=20, y=518)
         self.bill_frame.place(x=0, y=0)
         self.control_frame.place(x=0, y=0)
+        self.control_frame.grid_propagate(0)
 
         # -----------------------------------
 
@@ -106,7 +107,7 @@ class POS:
         # -----------------------------------
         self.price_display = Frame(
             self.control_frame, bd=3, relief=RIDGE, width=250, height=120, bg=BACKGROUND)
-        self.price_display.place(x=600, y=20)
+        self.price_display.grid(row=0, column=2, rowspan=3, padx=100, sticky=E)
 
         self.order_label = Label(
             self.control_frame, text="Order No", font=FONT, bg=BACKGROUND, fg=FOREGROUND)
@@ -122,14 +123,14 @@ class POS:
         self.order_but = Button(
             self.control_frame, text="Order", width=10, font=FONT1(12), bg=BACKGROUND, fg=WHITE, command=self.order)
 
-        self.order_but.place(x=600, y=160)
+        self.order_but.grid(row=3, column=2)
 
-        self.order_label.place(x=10, y=10)
-        self.table_label.place(x=10, y=60)
-        self.name_label.place(x=10, y=110)
-        self.order_entry.place(x=110, y=10)
-        self.table_entry.place(x=110, y=60)
-        self.name_entry.place(x=110, y=110)
+        self.order_label.grid(row=0, column=0, padx=15, sticky=W)
+        self.table_label.grid(row=1, column=0, padx=15, sticky=W)
+        self.name_label.grid(row=2, column=0, padx=15, sticky=W)
+        self.order_entry.grid(row=0, column=1, padx=15, sticky=W)
+        self.table_entry.grid(row=1, column=1, padx=15, sticky=W)
+        self.name_entry.grid(row=2, column=1, padx=15, sticky=W)
 
         # -----------------------------------
 
@@ -156,7 +157,7 @@ class POS:
 
         self.kot_but = Button(self.control_frame, text="KOT",
                               bg=BACKGROUND, fg=YELLOW, font=FONT1(20), command=self.kot)
-        self.kot_but.place(x=0, y=164)
+        self.kot_but.grid(row=3, column=0)
 
         # -----------------------------------
 
@@ -183,29 +184,34 @@ class POS:
     def add_item_bill(self, item):
         self.price = 0
         for i in range(len(self.selected_items)):
-            count = self.selected_items.count(self.selected_items[i])
+            self.count = self.selected_items.count(self.selected_items[i])
 
-            if count <= 1:
-                label1 = Label(self.items_miniframe,
-                               text=f"{self.selected_items[i][0]}", bg=BACKGROUND, fg=FOREGROUND, font=FONT)
-                label2 = Label(self.items_miniframe,
-                               text=f"{self.selected_items[i][1]}", bg=BACKGROUND, fg=FOREGROUND, font=FONT)
-                label3 = Label(self.items_miniframe,
-                               text=f"{self.selected_items[i][2]}", bg=BACKGROUND, fg=FOREGROUND, font=FONT)
-                self.label4 = Label(self.items_miniframe, text=f"{count}",
+            if self.count <= 1:
+                self.label1 = Label(self.items_miniframe,
+                                    text=f"{self.selected_items[i][0]}", bg=BACKGROUND, fg=FOREGROUND, font=FONT)
+                self.label2 = Label(self.items_miniframe,
+                                    text=f"{self.selected_items[i][1]}", bg=BACKGROUND, fg=FOREGROUND, font=FONT)
+                self.label3 = Label(self.items_miniframe,
+                                    text=f"{self.selected_items[i][2]}", bg=BACKGROUND, fg=FOREGROUND, font=FONT)
+                self.label4 = Label(self.items_miniframe, text=f"{self.count}",
                                     bg=BACKGROUND, fg=FOREGROUND, font=FONT)
-                button5 = Button(self.items_miniframe, text="x",
-                                 bg=BACKGROUND, fg=YELLOW, width=2, height=1,  font=FONT1(12), command=None)
-                button5.grid_propagate(0)
+                self.button5 = Button(self.items_miniframe, text="x", bg=BACKGROUND, fg=YELLOW, width=2, height=1,  font=FONT1(
+                    12), command=lambda j=self.selected_items[i]: self.remove(j))
+                self.button5.grid_propagate(0)
 
-                label1.grid(row=i+2, column=0, padx=20)
-                label2.grid(row=i+2, column=1, padx=20)
-                label3.grid(row=i+2, column=2, padx=20)
+                self.label6 = Label(self.items_miniframe, text=f"{self.count*self.selected_items[i][2]}",
+                                    bg=BACKGROUND, fg=FOREGROUND, font=FONT)
+
+                self.label1.grid(row=i+2, column=0, padx=20)
+                self.label2.grid(row=i+2, column=1, padx=20)
+                self.label3.grid(row=i+2, column=2, padx=20)
                 self.label4.grid(row=i+2, column=3, padx=20)
-                button5.grid(row=i+2, column=4)
+                self.button5.grid(row=i+2, column=4, padx=20)
+                self.label6.grid(row=i+2, column=5, padx=20)
 
             else:
-                self.label4["text"] = count
+                self.label4["text"] = self.count
+                self.label6["text"] = self.count*self.selected_items[i][2]
 
         self.items_miniframe.update()
         for i in self.selected_items:
@@ -329,3 +335,18 @@ class POS:
             self.window.destroy()
         except:
             pass
+
+    def remove(self, item):
+        self.selected_items.remove(item)
+        self.count -= 1
+        self.label4["text"] = f"{self.count}"
+        self.label6["text"] = self.count*item[2]
+
+        if self.count == 0:
+            self.label1.destroy()
+            self.label2.destroy()
+            self.label3.destroy()
+            self.label4.destroy()
+            self.button5.destroy()
+
+            self.label6.destroy()
